73bd8878a784a5d19f8c40bcdad05c526f8cf9ae
Boniface Bassey <boniface.bassey@helmut-fischer.com>
2023-03-08 16:41:54 +0100

    Added "Fluent" interface design pattern

---
+++ Creational-Design-Pattern/Fluent/FluentInterfaceDesignPattern/Employee.cs
@@ -0,0 +1,33 @@
+﻿namespace FluentInterfaceDesignPattern;
+
+public class Employee
+{
+    public string? FullName { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public string? Department { get; set; }
+    public string? Address { get; set; }
+
+    public Employee NameOfEmployee(string fullName)
+    {
+        FullName = fullName;
+        return this;
+    }
+
+    public Employee Born(string dateOfBirth)
+    {
+        DateOfBirth = Convert.ToDateTime(dateOfBirth);
+        return this;
+    }
+
+    public Employee WorkingOn(string department)
+    {
+        Department = department;
+        return this;
+    }
+
+    public Employee StaysAt(string address)
+    {
+        Address = address;
+        return this;
+    }
+}
---
+++ Creational-Design-Pattern/Fluent/FluentInterfaceDesignPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using FluentInterfaceDesignPattern;
+
+Employee employee = new Employee();
+
+var e = employee.NameOfEmployee("Spiritus Agustus")
+        .Born("01/11/1990")
+        .WorkingOn("Engineering")
+        .StaysAt("Nigeria");
+
+Console.WriteLine($"Fluent Employee:\n\t"
+                  + $"FullName: {e.FullName}\n\t"
+                  + $"DateOfBirth: {e.DateOfBirth}\n\t"
+                  + $"Department: {e.Department}\n\t"
+                  + $"Address: {e.Address}");
